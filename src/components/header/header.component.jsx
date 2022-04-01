efe416fefc29c0062769aa4d835b68067570d860
sava <sava666@mail.ru>
2022-04-01 10:08:15 +0300

    add item functionality, with grouping quantity

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -26,7 +26,7 @@ const Header = ({ currentUser, hidden }) => (
             }
             <CartIcon />
         </div>
-        {hidden && <CartDropdown />}
+        {hidden ? null : <CartDropdown />}
 
     </div>
 )
