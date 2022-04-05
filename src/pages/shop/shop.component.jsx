a093171319cc38f33a670014aca42f0ed93f728d
sava <sava666@mail.ru>
2022-04-05 12:56:11 +0300

    move directory and shop data to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,12 @@
 import React from "react";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
-import SHOP_DATA from './shop.data.js'
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state
-        return (
-            <div className='shop-page'>
-                {collections.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))}
-            </div>
-        )
-    }
-}
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component.jsx";
+import { Route } from "react-router-dom";
+import CollectionPage from "../collection/collection.component.jsx";
+const ShopPage = ({ match }) => (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+)
 
 export default ShopPage
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,22 @@
 import { combineReducers } from "redux";
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
 
-export default combineReducers({
+const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer,
 })
+
+export default persistReducer(persistConfig, rootReducer)
