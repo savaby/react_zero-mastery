e1532bda4d283a849043793bb82853295dde439a
sava <sava666@mail.ru>
2022-04-06 09:42:23 +0300

    firebase.config

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBQJIw4xoXjL1g7TaPJiwY7NxBEpskNRqw",
+    authDomain: "crwn-db-95b22.firebaseapp.com",
+    projectId: "crwn-db-95b22",
+    storageBucket: "crwn-db-95b22.appspot.com",
+    messagingSenderId: "481503526507",
+    appId: "1:481503526507:web:29fef0ffd60d4f5bab3b2e"
+};
+
+export default firebaseConfig
